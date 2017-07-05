d5873ed3cbebc3463781253f7c40b5cce26f5c7a
Ian Mccausland <imccausl@gmail.com>
2017-07-05 15:26:23 -0400

    Add simple, temporary test of pause function

--- src/app.js
+++ src/app.js
@@ -3,3 +3,7 @@ import Pomodoro from './modules/Pomodoro';
 const pomodoro = Pomodoro({ pomodoroLength: 1, breakLength: 1 });
 
 pomodoro.start(console.log);
+
+let pauseInterval = setTimeout(()=> {
+  pomodoro.pause();
+}, 2000);
