f031fa098d1b887b6d57aebc27d90948f4572947
Ian Mccausland <imccausl@gmail.com>
2017-07-06 09:40:29 -0400

    Testing pause/resume

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const pomodoro = Pomodoro({ pomodoroLength: 1, breakLength: 1 });
 
 pomodoro.start(console.log);
 
-let pauseInterval = setTimeout(()=> {
+setTimeout(()=> {
   pomodoro.pause();
 }, 2000);
 
-pauseInterval = setTimeout(()=> {
+setTimeout(()=> {
   pomodoro.start(console.log);
 }, 3000);
