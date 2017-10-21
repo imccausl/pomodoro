48d7387ebfdffc5788f4c7c319063186e423eea3
Ian McCausland <imccausl@gmail.com>
2017-10-20 22:40:25 -0400

    Fix conformity to style guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const webpack = require('webpack');
 
 module.exports = {
-    context: __dirname + '/src/app',
-    entry: {
-        app: './index.js'
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: '/node_modules/'
-            }
-        ]
-    }
+  context: `${__dirname}/src/app`,
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+    ],
+  },
 };
